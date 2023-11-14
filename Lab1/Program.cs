6f43218b332ee57edae780f8478d679c74dd79c7
l-BlackBird-l <olegkarp2014@gmail.com>
2023-11-14 18:36:16 +0200

    Add laboratory 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your birthday date is: {GetBirthdayDate(age + 1)} or  {GetBirthdayDate(age)}");
+
+            Console.ReadKey();
+        }
+
+        static int GetBirthdayDate(int age)
+        {
+            return DateTime.Now.Year - age;
+        }
+    }
+}
